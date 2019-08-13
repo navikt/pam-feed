a0375c362a31c4033751b9c768d1552201c16a4f
Øyvind Stegard <oyvind.stegard@nav.no>
2019-08-13 11:09:40 +0200

    [Fix] Shedlock defaultLockAtMostFor was not honoured, and upgrade Shedlock
    Pam-feed set defaultLockAtMostFor both through annotation `@EnableSchedulerLock`
    with a value of 60 seconds, and through dynamic configuration using
    `ScheduledLockConfiguration` bean. However, the latter was not honoured and all
    tasks got 60 seconds lock time at the most, even though config property
    `feed.duration` (used as value in lock config bean) was set to 10 (minutes).
    
    * Upgrade Shedlock. `ScheduledLockConfiguration` is now deprecated, so remove it
      and only use the default value from the annotation.
    
    * Also do some cleaning in POM dep management, upgrading required Spring
      components.

--- client/src/main/java/no/nav/pam/feed/taskscheduler/SchedulerConfig.java
+++ client/src/main/java/no/nav/pam/feed/taskscheduler/SchedulerConfig.java
@@ -1,36 +1,18 @@
 package no.nav.pam.feed.taskscheduler;
 
-import java.time.Duration;
-import javax.sql.DataSource;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
-import net.javacrumbs.shedlock.spring.ScheduledLockConfiguration;
-import net.javacrumbs.shedlock.spring.ScheduledLockConfigurationBuilder;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableScheduling
 @EnableSchedulerLock(defaultLockAtMostFor = "PT60S")
 public class SchedulerConfig {
-
-    @Bean
-    public ScheduledLockConfiguration scheduledLockConfiguration(
-        LockProvider lockProvider,
-        @Value("${feed.poolsize:10}") int poolSize,
-        @Value("${feed.duration:10}") long duration
-    ) {
-
-        return ScheduledLockConfigurationBuilder
-            .withLockProvider(lockProvider)
-            .withPoolSize(poolSize)
-            .withDefaultLockAtMostFor(Duration.ofMinutes(duration))
-            .build();
-
-    }
 
     @Bean
     public LockProvider lockProvider(DataSource dataSource) {
